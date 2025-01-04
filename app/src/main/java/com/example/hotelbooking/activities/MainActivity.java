1aa223d574e5d6d2974fe83da05478b2958a5568
Tuan <tuanchu24112004@gmail.com>
2025-01-04 10:42:44 +0700

    Set BottomSheetBehavior

--- app/src/main/java/com/example/hotelbooking/activities/MainActivity.java
+++ app/src/main/java/com/example/hotelbooking/activities/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.hotelbooking.activities;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
+import android.widget.LinearLayout;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -14,25 +14,19 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.hotelbooking.R;
-import com.example.hotelbooking.adapters.BestHotelAdapter;
+import com.example.hotelbooking.adapters.PopularDestinationAdapter;
 import com.example.hotelbooking.entities.PopularDestination;
-import com.example.hotelbooking.interfaces.AttractionApiService;
-import com.example.hotelbooking.response.PopularDestinationResponse;
-import com.example.hotelbooking.retrofit.RetrofitClient;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 import java.util.ArrayList;
-import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
     String baseUrl = "https://booking-com15.p.rapidapi.com/";
     String apiKey = "ec0b18b95emshe9254e3cb44c8d3p19bed8jsnffe7d747e56d";
     String apiHost = "booking-com15.p.rapidapi.com";
+    BottomSheetBehavior<LinearLayout> bottomSheetBehavior;
     private ArrayList<PopularDestination> items;
-    private BestHotelAdapter adapter;
+    private PopularDestinationAdapter adapter;
     private RecyclerView recyclerViewBestHotels;
 
     @Override
@@ -48,42 +42,89 @@ public class MainActivity extends AppCompatActivity {
         recyclerViewBestHotels = findViewById(R.id.recyclerViewBestHotels);
         recyclerViewBestHotels.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false));
         items = new ArrayList<>();
-        adapter = new BestHotelAdapter(this, items);
+        adapter = new PopularDestinationAdapter(this, items);
         recyclerViewBestHotels.setAdapter(adapter);
-        fetchBestHotels();
+        fetchPopularDestinations();
+
+
+        setBottomSheetBehavior();
     }
 
-    private void fetchBestHotels() {
-        AttractionApiService apiService = RetrofitClient.getClient(baseUrl).create(AttractionApiService.class);
-        apiService.searchAttractions(apiKey, apiHost, "vietnam")
-                .enqueue(new Callback<PopularDestinationResponse>() {
-                    @SuppressLint("NotifyDataSetChanged")
-                    @Override
-                    public void onResponse(@NonNull Call<PopularDestinationResponse> call, @NonNull Response<PopularDestinationResponse> response) {
-                        if (response.isSuccessful() && response.body() != null) {
-                            try {
-                                List<PopularDestination> data = response.body().getData();
-                                for (PopularDestination item : data) {
-                                    String imageUrl = item.getImageUrl();
-                                    String name = item.getName();
-                                    String city = item.getCityName();
-                                    String region = item.getRegion();
-                                    String location = city + ", " + region;
-                                    items.add(new PopularDestination(imageUrl, name, location, city, region));
-                                }
-                                adapter.notifyDataSetChanged();
-                                Log.d("d", "Items count: " + items.size());
+    private void fetchPopularDestinations() {
+        items.add(new PopularDestination(R.drawable.hanoi, "Hà Nội"));
+        items.add(new PopularDestination(R.drawable.hochiminh, "Hồ Chí Minh"));
+        items.add(new PopularDestination(R.drawable.hoian, "Hội An"));
+        items.add(new PopularDestination(R.drawable.danang, "Đà Nẵng"));
+        items.add(new PopularDestination(R.drawable.phuquoc, "Phú Quốc"));
+        items.add(new PopularDestination(R.drawable.nhatrang, "Nha Trang"));
+        items.add(new PopularDestination(R.drawable.hue, "Huế"));
+        items.add(new PopularDestination(R.drawable.ninhbinh, "Ninh Bình"));
+        adapter.notifyDataSetChanged();
+    }
 
-                            } catch (Exception e) {
-                                Log.e("API", "Error parsing JSON: " + e.getMessage());
-                            }
-                        }
-                    }
+    //    private void fetchBestHotels() {
+//        AttractionApiService apiService = RetrofitClient.getClient(baseUrl).create(AttractionApiService.class);
+//        apiService.searchAttractions(apiKey, apiHost, "vietnam")
+//                .enqueue(new Callback<PopularDestinationResponse>() {
+//                    @SuppressLint("NotifyDataSetChanged")
+//                    @Override
+//                    public void onResponse(@NonNull Call<PopularDestinationResponse> call, @NonNull Response<PopularDestinationResponse> response) {
+//                        if (response.isSuccessful() && response.body() != null) {
+//                            try {
+//                                List<PopularDestination> data = response.body().getData();
+//                                for (PopularDestination item : data) {
+//                                    String imageUrl = item.getImageUrl();
+//                                    String name = item.getName();
+//                                    String city = item.getCityName();
+//                                    String region = item.getRegion();
+//                                    String location = city + ", " + region;
+//                                    items.add(new PopularDestination(imageUrl, name, location, city, region));
+//                                }
+//                                adapter.notifyDataSetChanged();
+//                                Log.d("d", "Items count: " + items.size());
+//
+//                            } catch (Exception e) {
+//                                Log.e("API", "Error parsing JSON: " + e.getMessage());
+//                            }
+//                        }
+//                    }
+//
+//                    @Override
+//                    public void onFailure(@NonNull Call<PopularDestinationResponse> call, @NonNull Throwable t) {
+//                        Log.e("API", "Error: " + t.getMessage());
+//                    }
+//                });
+//    }
+    private void setBottomSheetBehavior() {
+        LinearLayout layoutRoomGuest = findViewById(R.id.layout_room_guest);
+        bottomSheetBehavior = BottomSheetBehavior.from(layoutRoomGuest);
 
-                    @Override
-                    public void onFailure(@NonNull Call<PopularDestinationResponse> call, @NonNull Throwable t) {
-                        Log.e("API", "Error: " + t.getMessage());
-                    }
-                });
+        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+
+        findViewById(R.id.checkin).setOnClickListener(v -> {
+            if (bottomSheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                layoutRoomGuest.setVisibility(View.VISIBLE);
+            } else {
+                bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+            }
+        });
+
+        // Xử lý callback khi thay đổi trạng thái
+        bottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                // Khi bị ẩn thì set visibility về GONE
+                if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
+                    layoutRoomGuest.setVisibility(View.GONE);
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                // Xử lý hiệu ứng khi vuốt (nếu cần)
+            }
+        });
     }
 }
--- app/src/main/java/com/example/hotelbooking/entities/PopularDestination.java
+++ app/src/main/java/com/example/hotelbooking/entities/PopularDestination.java
@@ -1,50 +1,21 @@
 package com.example.hotelbooking.entities;
 
 public class PopularDestination {
-    private String image_url;
-    private String name;
+    private int image_url;
     private String location;
-    private String city_name;
-    private String region;
 
-    public String getRegion() {
-        return region;
-    }
 
-    public void setRegion(String region) {
-        this.region = region;
-    }
-
-    public String getCityName() {
-        return city_name;
-    }
-
-    public void setCityName(String cityName) {
-        this.city_name = cityName;
-    }
-
-    public PopularDestination(String imageUrl, String nameHotel, String location, String cityName, String region) {
+    public PopularDestination(int imageUrl, String location) {
         this.image_url = imageUrl;
-        this.name = nameHotel;
         this.location = location;
-        this.city_name = cityName;
-        this.region = region;
     }
 
-    public String getImageUrl() {
+    public int getImageUrl() {
         return image_url;
     }
 
-    public void setImageUrl(String imageUrl) {
+    public void setImageUrl(int imageUrl) {
         this.image_url = imageUrl;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public String getLocation() {
